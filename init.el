15979002cf9a0d9de60d4da300e16150b7135337
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2023-04-28 08:27:36 -0600

    Update init.el

--- init.el
+++ init.el
@@ -30,6 +30,16 @@
 (show-paren-mode 1)
 (recentf-mode 1)
 
+(defun set-exec-path-from-shell ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell)
+
 ;; My customized packages
 (add-to-list 'load-path "~/.emacs.d/rf")
 (require 'funcs)
