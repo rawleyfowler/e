fd770e6c22efc8fd25ced36ed737f50b94ae0a2d
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2023-05-19 21:30:22 -0600

    Update docs to fix flymake, add open-in-firefox command

--- rf/funcs.el
+++ rf/funcs.el
@@ -4,24 +4,30 @@
 ;;; Code:
 
 (defun rf/indent-buffer ()
-  "Apply indenting to whole buffer"
+  "Apply indenting to whole buffer."
   (interactive)
   (save-excursion
     (indent-region (point-min) (point-max) nil)))
 
 (defun rf/goto-dashboard ()
-  "Go back to dashboard buffer"
+  "Go back to dashboard buffer."
   (interactive)
   (switch-to-buffer "*dashboard*")
   (dashboard-mode)
   (dashboard-refresh-buffer))
 
 (defun rf/kill-inner-word ()
-  "ciw from v*m, in Emacs"
+  "It's ciw from Vim."
   (interactive)
   (forward-char 1)
   (backward-word)
   (kill-word 1))
+
+(defun rf/open-in-firefox ()
+  "Attempts to open the current buffers file in Firefox."
+  (interactive)
+  (setq-local browser-file (concat (file-name-sans-extension (buffer-file-name))))
+  (shell-command (concat "firefox " browser-file)))
 
 (provide 'funcs)
 ;;; funcs.el ends here
