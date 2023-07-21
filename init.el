1c091dc3cf0ad17f3fe387ec191bb0d7d5909c14
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2023-07-21 14:11:19 -0600

    Change theme

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;;; init.el --- Perl 5 Emacs Configuration
 ;;; Commentary:
 ;;; A Perl 5 focused Emacs configuration, for the unix genie.
@@ -107,7 +106,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tsdh-light))
+ '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
    '("fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" default))
  '(delete-selection-mode nil)
